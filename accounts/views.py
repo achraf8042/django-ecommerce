ff249a48e85b0bb369d21900567aadb0ee97e7c5
salmyachraf <salmyachraf@gmail.com>
2025-01-30 12:35:00 +0100

    fix some issues

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def user_signup(request);
+    return render(request, 'account/signup.html')
+
+def user_login(request);
+    return render(request, 'account/login.html')
